8b1c4613a3d27465028e73695fb62c03401abc09
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2025-02-22 14:23:18 +0500

    added lab1 and lab2

---
+++ Lab1/Lab1/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            displayBox1 = new TextBox();
+            button17 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(25, 126);
+            button1.Name = "button1";
+            button1.Size = new Size(42, 39);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaption;
+            button2.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(155, 171);
+            button2.Name = "button2";
+            button2.Size = new Size(42, 39);
+            button2.TabIndex = 1;
+            button2.Text = "6";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaption;
+            button3.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(225, 171);
+            button3.Name = "button3";
+            button3.Size = new Size(42, 39);
+            button3.TabIndex = 2;
+            button3.Text = "-";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ActiveCaption;
+            button4.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.ActiveCaptionText;
+            button4.Location = new Point(86, 171);
+            button4.Name = "button4";
+            button4.Size = new Size(42, 39);
+            button4.TabIndex = 3;
+            button4.Text = "5";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ActiveCaption;
+            button5.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.ForeColor = SystemColors.ActiveCaptionText;
+            button5.Location = new Point(25, 171);
+            button5.Name = "button5";
+            button5.Size = new Size(42, 39);
+            button5.TabIndex = 4;
+            button5.Text = "4";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ActiveCaption;
+            button6.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.ForeColor = SystemColors.ActiveCaptionText;
+            button6.Location = new Point(225, 126);
+            button6.Name = "button6";
+            button6.Size = new Size(42, 39);
+            button6.TabIndex = 5;
+            button6.Text = "+";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.ActiveCaption;
+            button7.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.ForeColor = SystemColors.ActiveCaptionText;
+            button7.Location = new Point(155, 126);
+            button7.Name = "button7";
+            button7.Size = new Size(42, 39);
+            button7.TabIndex = 6;
+            button7.Text = "3";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.ActiveCaption;
+            button8.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.ForeColor = SystemColors.ActiveCaptionText;
+            button8.Location = new Point(86, 126);
+            button8.Name = "button8";
+            button8.Size = new Size(42, 39);
+            button8.TabIndex = 7;
+            button8.Text = "2";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.ActiveCaption;
+            button9.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button9.ForeColor = SystemColors.ActiveCaptionText;
+            button9.Location = new Point(225, 216);
+            button9.Name = "button9";
+            button9.Size = new Size(42, 39);
+            button9.TabIndex = 8;
+            button9.Text = "*";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = SystemColors.ActiveCaption;
+            button10.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button10.ForeColor = SystemColors.ActiveCaptionText;
+            button10.Location = new Point(155, 216);
+            button10.Name = "button10";
+            button10.Size = new Size(42, 39);
+            button10.TabIndex = 9;
+            button10.Text = "9";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = SystemColors.ActiveCaption;
+            button11.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button11.ForeColor = SystemColors.ActiveCaptionText;
+            button11.Location = new Point(86, 216);
+            button11.Name = "button11";
+            button11.Size = new Size(42, 39);
+            button11.TabIndex = 10;
+            button11.Text = "8";
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.BackColor = SystemColors.ActiveCaption;
+            button12.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button12.ForeColor = SystemColors.ActiveCaptionText;
+            button12.Location = new Point(25, 216);
+            button12.Name = "button12";
+            button12.Size = new Size(42, 39);
+            button12.TabIndex = 11;
+            button12.Text = "7";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.BackColor = SystemColors.ActiveCaption;
+            button13.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button13.ForeColor = SystemColors.ActiveCaptionText;
+            button13.Location = new Point(225, 261);
+            button13.Name = "button13";
+            button13.Size = new Size(42, 39);
+            button13.TabIndex = 12;
+            button13.Text = "/";
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            button14.BackColor = SystemColors.ActiveCaption;
+            button14.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button14.ForeColor = SystemColors.ActiveCaptionText;
+            button14.Location = new Point(155, 261);
+            button14.Name = "button14";
+            button14.Size = new Size(42, 39);
+            button14.TabIndex = 13;
+            button14.Text = "AC";
+            button14.UseVisualStyleBackColor = false;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.BackColor = SystemColors.ActiveCaption;
+            button15.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button15.ForeColor = SystemColors.ActiveCaptionText;
+            button15.Location = new Point(86, 261);
+            button15.Name = "button15";
+            button15.Size = new Size(42, 39);
+            button15.TabIndex = 14;
+            button15.Text = ".";
+            button15.UseVisualStyleBackColor = false;
+            button15.Click += button15_Click;
+            // 
+            // button16
+            // 
+            button16.BackColor = SystemColors.ActiveCaption;
+            button16.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button16.ForeColor = SystemColors.ActiveCaptionText;
+            button16.Location = new Point(25, 261);
+            button16.Name = "button16";
+            button16.Size = new Size(42, 39);
+            button16.TabIndex = 15;
+            button16.Text = "0";
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += button16_Click;
+            // 
+            // displayBox1
+            // 
+            displayBox1.Location = new Point(25, 78);
+            displayBox1.Name = "displayBox1";
+            displayBox1.Size = new Size(242, 23);
+            displayBox1.TabIndex = 16;
+            displayBox1.TextAlign = HorizontalAlignment.Right;
+            displayBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button17
+            // 
+            button17.BackColor = SystemColors.ActiveCaption;
+            button17.Font = new Font("SimSun", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button17.ForeColor = SystemColors.ActiveCaptionText;
+            button17.Location = new Point(98, 306);
+            button17.Name = "button17";
+            button17.Size = new Size(86, 32);
+            button17.TabIndex = 17;
+            button17.Text = "=";
+            button17.UseVisualStyleBackColor = false;
+            button17.Click += button17_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(292, 361);
+            Controls.Add(button17);
+            Controls.Add(displayBox1);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Calculator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private TextBox displayBox1;
+        private Button button17;
+    }
+}
